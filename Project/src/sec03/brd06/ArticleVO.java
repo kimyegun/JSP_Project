9cde720dd972e0447d475a294440b25e6c8741da
KimYeGun <kimyegun123@gmail.com>
2023-06-06 11:58:03 +0900

    글 삭제 기능 구현

---
+++ Project/src/sec03/brd06/ArticleVO.java
@@ -0,0 +1,126 @@
+package sec03.brd06;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.sql.Date;
+
+public class ArticleVO {
+	private int level;
+	private int articleNO;
+	private int parentNO;
+	private String title;
+	private String content;
+	private String imageFileName;
+	private String id;
+	private Date writeDate;
+	
+	public ArticleVO() {
+		
+	}
+
+
+	public ArticleVO(int level, int articleNO, int parentNO, String title, String content, String imageFileName,
+			String id) {
+		super();
+		this.level = level;
+		this.articleNO = articleNO;
+		this.parentNO = parentNO;
+		this.title = title;
+		this.content = content;
+		this.imageFileName = imageFileName;
+		this.id = id;
+	}
+
+
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	public int getArticleNO() {
+		return articleNO;
+	}
+
+
+	public void setArticleNO(int articleNO) {
+		this.articleNO = articleNO;
+	}
+
+
+	public int getParentNO() {
+		return parentNO;
+	}
+
+
+	public void setParentNO(int parentNO) {
+		this.parentNO = parentNO;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+
+	public String getImageFileName() {
+		return imageFileName;
+	}
+
+	public void setImageFileName(String imageFileName) {
+		try {
+			if(imageFileName!=null && imageFileName.length()!=0) {
+				this.imageFileName = URLEncoder.encode(imageFileName, "UTF-8");  //파일이름에 특수문자가 있을 경우 인코딩합니다.
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public Date getWriteDate() {
+		return writeDate;
+	}
+
+
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+	
+	
+	
+	
+	
+
+}
